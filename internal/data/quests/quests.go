2c6680d742a0a204175264c1f5dc18eebd9a0d67
MyouM <youssef95@mail.ru>
2025-07-03 18:46:20 +0300

    v0.1

---
+++ internal/data/quests/quests.go
@@ -0,0 +1,28 @@
+package quests
+
+type Quest struct {
+	Text     string
+	Need     []string
+	QuestNum int
+}
+
+var allQuests = []Quest{
+	{
+		Text:     "надо собрать рюкзак и идти в универ. ",
+		Need:     []string{"рюкзак", "конспекты",},
+		QuestNum: 0,
+	},
+	{
+		Text:     "надо идти в универ. ",
+		Need:     []string{"гранит",},
+		QuestNum: 1,
+	},
+}
+
+func (q *Quest) ChangeQuest() {
+	*q = allQuests[q.QuestNum+1]
+}
+
+func InitQuests() Quest {
+	return allQuests[0]
+}
